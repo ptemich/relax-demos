f6d4cb9e42eae0963f16582db0a2d980c2bd1caa
przemek <ptemich@gmail.com>
2026-08-20 09:48:35 +0200

    j25

---
+++ src/test/java/org/relax/GatherersTest.java
@@ -0,0 +1,31 @@
+package org.relax;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Gatherers;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GatherersTest {
+
+    @Test
+    public void test1() {
+        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5);
+        //Stream gathered = numbers.gather(Gatherers.fold(() -> 0, Integer::sum));
+        Stream gathered = numbers.gather(Gatherers.scan(() -> 0, Integer::sum));
+
+        List<Integer> resultList = gathered.toList();
+        assertThat(resultList).hasSize(1);
+        assertThat(resultList.getFirst()).isEqualTo(Integer.valueOf(15));
+
+
+
+        Stream<String> strings = Stream.of("pierwszy", "drugi", "trzeci");
+        Stream<String> gatheredStreams = strings.gather(Gatherers.fold(() -> "paczatek", String::concat));
+        List<String> stringsList = gatheredStreams.toList();
+        assertThat(stringsList).hasSize(1);
+    }
+
+}
