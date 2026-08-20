ad2259b66234b7f2880521379440bc7134484ab4
przemek <ptemich@gmail.com>
2026-08-20 11:54:33 +0200

    small adjustments to rendering algorithm

---
+++ src/main/java/org/relax/entities/Missile.java
@@ -0,0 +1,24 @@
+package org.relax.entities;
+
+import org.relax.Game;
+import org.relax.Sprite;
+
+import java.awt.*;
+
+public class Missile extends Sprite {
+
+    public Missile(Game game, int x, int y) {
+        super(game, x, y);
+    }
+
+    @Override
+    public void render(Graphics2D g2d) {
+
+    }
+
+    @Override
+    public void update(double elapsedTime_ns) {
+
+    }
+
+}
